cbd5e9c7ea727927f7adb3eaa62e91a891e9a482
RicoWilliams <ricowilliams@protonmail.com>
2021-11-22 12:05:11 -0700

    fixed cardArray format

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const cards = document.querySelectorAll('.card');
 
 //use my flipCard function in a loop to iterate through all cards
 //This should change the cardFront from original image to the other image
-let cardFront =
+let cardFrontOne =
   'http://images.folksy.com/aXRlbXMvMzczODc3LzIwMTQwNzA2LzE5NDg1MDk4OTAxNw-Q-Q-N/gallery/6446011-Hiking-Boots-0';
 let cardBack =
   'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.customerparadigm.com%2Fwp-content%2Fuploads%2F2011%2F06%2Fflatirons-june23-2011-09271-150x150.jpg&f=1&nofb=1';
@@ -28,46 +28,19 @@ let cardFrontNine =
   'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.boundarywaterscatalog.com%2Fprodimages%2F1358-DEFAULT-s.jpg&f=1&nofb=1';
 
 //Array of card options, with front images and their links
-const cardArray = [
-  {
-    name: 'hikingBoots',
-    img: 'http://images.folksy.com/aXRlbXMvMzczODc3LzIwMTQwNzA2LzE5NDg1MDk4OTAxNw-Q-Q-N/gallery/6446011-Hiking-Boots-0'
-  },
-  {
-    name: 'hikingPack',
-    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.for-sale.co.uk%2Fsh-img%2FGRAGRE_osprey%25252Batmos%25252B65_thumbnail.jpg&f=1&nofb=1'
-  },
-  {
-    name: 'hikingMap',
-    img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fphotos.gograph.com%2Fthumbs%2FCSP%2FCSP386%2Fk20899317.jpg&f=1&nofb=1'
-  },
-  {
-    name: 'hikingCompass',
-    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-grid.fotosearch.com%2FCSP%2FCSP739%2Fcompas-clip-art__k7396786.jpg&f=1&nofb=1'
-  },
-  {
-    name: 'hikingPants',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/31hSfQHPrZL._AC_US160_.jpg'
-  },
-  {
-    name: 'hikingShirt',
-    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F61aq3sDldgL._AC_UL160_SR160%2C160_.jpg&f=1&nofb=1'
-  },
-  {
-    name: 'hikingBottle',
-    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1296%2F9397%2Fproducts%2Faqua_45_compact.jpg%3Fv%3D1531945161&f=1&nofb=1'
-  },
-  {
-    name: 'hikingStove',
-    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.boundarywaterscatalog.com%2Fprodimages%2F1358-DEFAULT-s.jpg&f=1&nofb=1'
-  },
-  {
-    name: 'hikingTent',
-    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fwww.ozark-trail-tents.com%2Fwp-content%2Fuploads%2F2009%2F07%2FOzarkTrailTents01.jpg&f=1&nofb=1'
-  }
+let cardArray = [
+  cardFrontOne,
+  cardFrontTwo,
+  cardFrontThree,
+  cardFrontFour,
+  cardFrontFive,
+  cardFrontSix,
+  cardFrontSeven,
+  cardFrontEight,
+  cardFrontNine
 ];
 
-//shuffle function for card array(fisher-yates)
+//shuffle function for card array(fisher-yates), clicking start game should shuffle the cards
 function cardShuffle(cardArray) {
   let startingIndex = cardArray.length,
     random;
@@ -100,20 +73,17 @@ for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', () => flipCard(img));
 }
 //function to change img of cards when clicked
+// let cardFront = cardArray;
 function flipCard(img) {
   if (img.src === cardBack) {
-    img.src = cardFront;
+    img.src = cardArray;
     console.log('flipped');
     return;
   }
   img.src = cardBack;
-  function delayCards() {
-    delay = setTimeout(cards);
-  }
 }
 //check if cards match, if so they should remain with clicked img
 let cardsSelected = [];
-let cardName = [];
 function winMatches() {
   let firstCard = cardArray[0];
   let secondCard = cardArray[1];
@@ -123,5 +93,9 @@ function winMatches() {
     if (cardArray[0] !== cardArray[1]) {
       img.src = cardBack;
     }
+    return;
   }
+}
+function resetGame() {
+  document.getElementById('board').reset();
 }
