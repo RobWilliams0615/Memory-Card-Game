bf7bc1e6a8d5e0b8ba8fecc6100711449fd35300
RicoWilliams <ricowilliams@protonmail.com>
2021-11-22 19:53:35 -0700

    removed code used for testing. cleaned up code

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 //Variables
 const replay = document.querySelector('button');
-// const startGame = document.querySelector('#startGame');
 const homePage = document.querySelector('#homePage');
 const cards = document.querySelectorAll('.card');
-const cardBack =
-  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.customerparadigm.com%2Fwp-content%2Fuploads%2F2011%2F06%2Fflatirons-june23-2011-09271-150x150.jpg&f=1&nofb=1';
 let numOfClicks = 0;
 let cardsSelected = [];
-//use my flipCard function in a loop to iterate through all cards
-//This should change the cardFront from original image to the other image
-
 //Array of card options, with front images and their links
-
 let cardArray = [
   'boots',
   'boots',
@@ -33,7 +26,7 @@ let cardArray = [
   'stove'
 ];
 
-//shuffle function for card array(fisher-yates), clicking start game should shuffle the cards
+//shuffle function for card array
 const cardShuffle = (cardArray) => {
   for (let i = cardArray.length - 1; i > 0; i--) {
     const a = Math.floor(Math.random() * (i + 1));
@@ -41,23 +34,10 @@ const cardShuffle = (cardArray) => {
     cardArray[i] = cardArray[a];
     cardArray[a] = b;
   }
-  // for (let i = cardArray.length; i > 0; i--) {
-  //   cards[i].style.backgroundColor = 'yellow';
-  //   cards[i].style.color = 'yellow';
-  //   numOfClicks = 0;
-  // }
 };
 cardShuffle(cardArray);
-
+//setting back of cards and inner text to yellow, event listener for cards to flip
 for (let i = 0; i < cards.length; i++) {
-  let back = document.createElement('img');
-  back.src = cardBack;
-  back.classList = 'back';
-  // cards[i].appendChild(back);
-  let img = document.createElement('img');
-  img.src = cardArray[Math.floor(Math.random() * cardArray.length)];
-  // img.classList = 'front';
-  // img.style.display = 'none';
   cards[i].innerHTML = cardArray[i]
     ? cardArray[i]
     : cardArray[cardArray.length - 1 - i];
@@ -80,16 +60,13 @@ function flipCard(card) {
   }
 }
 
-// check if cards match, if so they should remain with clicked img, if not they go back to starting color
+// check if cards match, if so they should remain green, if not they go back to yellow
 function winMatches() {
   let card1 = cardsSelected[0];
   let card2 = cardsSelected[1];
   if (card1.innerText === card2.innerText) {
-    setTimeout(() => {
-      // card1.style.opacity = '0';
-      // card2.style.opacity = '0';
-      alert('Match');
-    }, 500);
+    card1.style.backgroundColor = 'green';
+    card2.style.backgroundColor = 'green';
   } else {
     if (card1.innerText !== card2.innerText) {
       card1.style.backgroundColor = 'yellow';
@@ -106,5 +83,6 @@ function winMatches() {
 function resetCards() {
   location.reload();
 }
+//Event listener for replay game button
 const resetButton = document.querySelector('#replayButton');
 resetButton.addEventListener('click', resetCards);
