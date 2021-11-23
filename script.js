02f2c820e483f3600b8c7aac97a31b60587a42dc
RicoWilliams <ricowilliams@protonmail.com>
2021-11-22 18:04:22 -0700

    Replay Game button now resets cards to original state

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Variables
-const replay = document.querySelector('#replayGame');
+const replay = document.querySelector('button');
 // const startGame = document.querySelector('#startGame');
 const homePage = document.querySelector('#homePage');
 const cards = document.querySelectorAll('.card');
@@ -11,6 +11,7 @@ let cardsSelected = [];
 //This should change the cardFront from original image to the other image
 
 //Array of card options, with front images and their links
+
 let cardArray = [
   'boots',
   'boots',
@@ -18,28 +19,20 @@ let cardArray = [
   'lighter',
   'tent',
   'tent',
-  'bug spray',
-  'bug spray',
+  'hiking shirt',
+  'hiking shirt',
   'knife',
   'knife',
-  'box1',
-  'box1',
-  'box2',
-  'box2',
-  'box3',
-  'box3',
-  'box4',
-  'box4'
-  // 'https://images-na.ssl-images-amazon.com/images/I/61dw5Hjoj8L.jpg',
-  // 'http://images.folksy.com/aXRlbXMvMzczODc3LzIwMTQwNzA2LzE5NDg1MDk4OTAxNw-Q-Q-N/gallery/6446011-Hiking-Boots-0',
-  // 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fphotos.gograph.com%2Fthumbs%2FCSP%2FCSP386%2Fk20899317.jpg&f=1&nofb=1',
-  // 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-grid.fotosearch.com%2FCSP%2FCSP739%2Fcompas-clip-art__k7396786.jpg&f=1&nofb=1',
-  // 'https://images-na.ssl-images-amazon.com/images/I/31hSfQHPrZL._AC_US160_.jpg',
-  // 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F61aq3sDldgL._AC_UL160_SR160%2C160_.jpg&f=1&nofb=1',
-  // 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fwww.ozark-trail-tents.com%2Fwp-content%2Fuploads%2F2009%2F07%2FOzarkTrailTents01.jpg&f=1&nofb=1',
-  // 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1296%2F9397%2Fproducts%2Faqua_45_compact.jpg%3Fv%3D1531945161&f=1&nofb=1',
-  // 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.boundarywaterscatalog.com%2Fprodimages%2F1358-DEFAULT-s.jpg&f=1&nofb=1'
+  'water bottle',
+  'water bottle',
+  'hiking pants',
+  'hiking pants',
+  'compass',
+  'compass',
+  'stove',
+  'stove'
 ];
+
 //shuffle function for card array(fisher-yates), clicking start game should shuffle the cards
 const cardShuffle = (cardArray) => {
   for (let i = cardArray.length - 1; i > 0; i--) {
@@ -48,20 +41,24 @@ const cardShuffle = (cardArray) => {
     cardArray[i] = cardArray[a];
     cardArray[a] = b;
   }
+  // for (let i = cardArray.length; i > 0; i--) {
+  //   cards[i].style.backgroundColor = 'yellow';
+  //   cards[i].style.color = 'yellow';
+  //   numOfClicks = 0;
+  // }
 };
-// cardShuffle(cardArray);
-// document.getElementById('startGame').addEventListener('click', cardShuffle);
+cardShuffle(cardArray);
 
 for (let i = 0; i < cards.length; i++) {
   let back = document.createElement('img');
   back.src = cardBack;
   back.classList = 'back';
   // cards[i].appendChild(back);
-  // let img = document.createElement('img');
-  // img.src = cardArray[Math.floor(Math.random() * cardArray.length)];
+  let img = document.createElement('img');
+  img.src = cardArray[Math.floor(Math.random() * cardArray.length)];
   // img.classList = 'front';
   // img.style.display = 'none';
-  cards[i].innerText = cardArray[i]
+  cards[i].innerHTML = cardArray[i]
     ? cardArray[i]
     : cardArray[cardArray.length - 1 - i];
   cards[i].style.backgroundColor = 'yellow';
@@ -69,7 +66,7 @@ for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', () => flipCard(cards[i]));
 }
 
-//function to change img of cards when clicked
+//function to change color of cards when clicked
 function flipCard(card) {
   card.style.backgroundColor = 'green';
   card.disabled = true;
@@ -106,5 +103,8 @@ function winMatches() {
   cardsSelected = [];
   numOfClicks = 0;
 }
-
-// get flip card to work and dont worry about images display nothing, display something
+function resetCards() {
+  location.reload();
+}
+const resetButton = document.querySelector('#replayButton');
+resetButton.addEventListener('click', resetCards);
